d2976bda539d8cdd57047501e29719c69768d7f5
sazzadtanim <tanimsazzad@gmail.com>
2017-06-22 12:41:51 +0600

    some experiments

--- app.js
+++ app.js
@@ -2,10 +2,24 @@
 
 var http = require('http');
 var fs = require('fs');
-
+var filename = process.argv[2];
 
 var server  = http.createServer(function(connection){
-  console.log('someone connected to your server');
+  // ekhane amar ekta question ache?
+  //  function er vitore connection er kaj ki?
+  // eita janar jonno
+  // console.log(connection);
+
+  console.log('Someone is connected to your server.\n'+'He is watching filename: '+ filename+' for changes..\n');
+  // main kajta korte hobe , file take watch korte hobe
+  var watcher = fs.watch(filename, function(){
+    console.log('file has been changed.');
+  });
+  // connection close hole janabe
+  connection.on('close', function(){
+    console.log('user has left.');
+  });
+
 });
 
 
